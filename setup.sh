aa31aa337b594d4b586f64630c7cf5de464217d1
semanursancar <74814573+semanursancar@users.noreply.github.com>
2023-02-16 16:15:01 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update package lists and install Tesseract OCR
-apt-get update
-apt-get install -y tesseract-ocr
-
-# Add Tesseract OCR to PATH
-echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
